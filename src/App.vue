8dc408e382a8a9c733d56c36c22d3b7b2b41a22b
KJTempas <fw6927mm@go.minneapolis.edu>
2020-03-05 14:18:49 -0600

    added questions; Answers.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
-    <WouldYouRather></WouldYouRather>>
+    <WouldYouRatherQuestion>v-for="question in questions</WouldYouRatherQuestion>
 
     
   </div>
 </template>
 
 <script>
-import WouldYouRatherQuestion from './components/WouldYouRatherQuestion.vue'WouldYouRatherQuestion
+import WouldYouRatherQuestion from './components/WouldYouRatherQuestion.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: 'be a master at drawing or be an amazing singer?',
+          answer1: 'Be a master at drawing',
+          answer2: 'Be an amazing singer',
+        },
+        {
+          id: 1,
+          question: 'be a famous inventor or a famous writewr?',
+          answer1: 'Be a famous inventor',
+          answe2: 'Be a famous writer',
+        },
+        {
+          id: 2,
+          question: 'be able to type/text very fast or be able to read really quickly?',
+          answer1: 'Be able to type/text very fast',
+          answer2: 'Be able to read really quickly'
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    WouldYouRatherQuestion
   }
 }
 </script>
---
+++ src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <!-- template goes here -->
+ <h1> Would you rather...</h1>
+
+        <would-you-rather-question
+            v-bind:question="question"
+            v-bind:answer1="answer1"
+            v-bind:answer2="answer2"
+            v-on:answer-changed="answerChanged">
+
+        </would-you-rather-question>
+<!--note: component name is generated from the component, but lowercase w/ hyphens; or camelCase-->
+    <div>
+        {{ userSelectionMessage }}
+    </div>
+
+
+
+        </div>
+</template>
+
+
+<script>
+export default {
+    //create and export component here
+    name: 'WouldYouRatherQuestion',
+    date() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
